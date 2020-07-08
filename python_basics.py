c7cb1a564f11d1305ea83a1669d241f2622ee086
canantu <67923214+canantu@users.noreply.github.com>
2020-07-08 22:41:03 +0200

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Write a code that outputs your name and surname in one line.
+# 1. Write a 1-line code that outputs your name and surname in two separate lines by using a single print() call.
+# 1. Use print() function that returns the following text. Notice the quotes in the text. Those quotes should be seen in the output!:  
+#  `I don't want to be an "artist". I want to be a "Data Scientist."`
+
+# In[ ]:
+
+
+print('canan tunc')
+
+
+# In[2]:
+
+
+print('canan\ntunc')
+
+
+# In[5]:
+
+
+print('''I don't want to be an "artist". I want to be a "Data Scientist."''')
+
+
+# In[ ]:
+
+
+
+
